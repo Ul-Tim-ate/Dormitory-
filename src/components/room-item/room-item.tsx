600c4c033334c7834090a7b23aa6169a1b142e06
Timofey Trakhanov <timohator1@mail.ru>
2022-11-30 08:33:15 +0300

    mend

---
+++ src/components/room-item/room-item.tsx
@@ -0,0 +1,23 @@
+import React, { FC } from "react";
+import MyFillButton from "../UI/fill-button/MyFillButton";
+import "./room-item.sass";
+
+interface RoomItemProps {
+  roomNumber: number;
+  free: number;
+  buzy: number;
+}
+
+const RoomItem: FC<RoomItemProps> = ({ roomNumber, free, buzy }) => {
+  return (
+    <li className="room-item">
+      <h3 className="room-item__header">Комната {roomNumber}</h3>
+      <span className="room-item__info">
+        Свободных мест: {buzy} из {free}
+      </span>
+      <MyFillButton>Заселить</MyFillButton>
+    </li>
+  );
+};
+
+export default RoomItem;
---
+++ src/components/room-list/room-list.tsx
@@ -0,0 +1,17 @@
+import React from "react";
+import RoomItem from "../room-item/room-item";
+import "./room-list.sass";
+
+const RoomList = () => {
+  return (
+    <ul className="room-list">
+      <RoomItem buzy={1} free={2} roomNumber={394} />
+      <RoomItem buzy={1} free={2} roomNumber={394} />
+      <RoomItem buzy={1} free={2} roomNumber={394} />
+      <RoomItem buzy={1} free={2} roomNumber={394} />
+      <RoomItem buzy={1} free={2} roomNumber={394} />
+    </ul>
+  );
+};
+
+export default RoomList;
