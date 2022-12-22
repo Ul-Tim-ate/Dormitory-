997a5b089d3fd9f76da070e43551640b3e88e868
Ul-Tim-ate <68898246+Ul-Tim-ate@users.noreply.github.com>
2022-12-22 11:52:12 +0300

    Add files via upload

---
+++ src/components/header/header.tsx
@@ -0,0 +1,56 @@
+import React from "react";
+import logo from "./logo.svg";
+import logout from "./logout.svg";
+import user from "./user-img.jpg";
+import "./header.sass";
+import { useDispatch } from "react-redux";
+import { authLogoutAction } from "../../store/actions/auth-actions";
+import { useNavigate } from "react-router-dom";
+import { useTypedSelector } from "../../hooks/use-typed-selector";
+import { IUser } from "../../models/user";
+
+const Header = () => {
+  const dispatch = useDispatch();
+  const navigate = useNavigate();
+  const authUser: IUser = useTypedSelector(
+    (state) => state.authReducer.user.authUser
+  );
+  return (
+    <header className="header">
+      <div className="container">
+        <div className="header-wrapper">
+          <a href="#!" className="header__href">
+            <img src={logo} alt="Логотип MyDormitry" className="header__logo" />
+          </a>
+          <ul className="header__buttons">
+            <li
+              className="header__button"
+              onClick={() => {
+                navigate(`/auth/user/${authUser.id}`);
+              }}
+            >
+              <img
+                src={user}
+                alt="Фотография пользователя"
+                className="header__user"
+              />
+            </li>
+            <li className="header__button">
+              <img
+                src={logout}
+                alt="Выйти из аккаунта"
+                className="header__logout"
+                onClick={() => {
+                  dispatch(authLogoutAction());
+                  navigate("/");
+                }}
+              />
+            </li>
+          </ul>
+        </div>
+      </div>
+    </header>
+  );
+};
+
+export default Header;
