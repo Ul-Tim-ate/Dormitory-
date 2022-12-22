5fdf0ccfad740beebed1db54a41fe90af1544a82
Timofey Trakhanov <timohator1@mail.ru>
2022-12-22 11:37:03 +0300

    rename folder app

--- src/index.tsx
+++ src/index.tsx
@@ -2,10 +2,10 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.sass";
 import "./reset.sass";
-import App from "./components/App/app";
 import { BrowserRouter } from "react-router-dom";
 import { Provider } from "react-redux";
 import store from "./store";
+import App from "./components/app/app";
 
 const root = ReactDOM.createRoot(
   document.getElementById("root") as HTMLElement
